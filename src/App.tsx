44f006f52bd99f565687fbe59fad39d68b2da3ef
minhos <minhos3389@gmail.com>
2024-04-26 00:20:59 +0900

    apply useRef to setValue

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import Button from "./components/Button";
 
 function App() {
   const ref = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState("");
 
+  // dont render every moments when put text to input element
   const handleClick = () => {
     if (ref.current) {
+      setValue(ref.current.value);
       ref.current.focus();
-      console.log("id", ref.current.id); // id one
     }
   };
 
   return (
     <div>
       <input id="one" ref={ref} />
-      <button onClick={handleClick}>focus</button>
+      <button onClick={handleClick}>show value</button>
+      <p>value: {value}</p>
     </div>
   );
 }
